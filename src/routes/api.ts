2fef1775c839079f8e1085ef06c208724157eb7f
99slayer <21979862+99slayer@users.noreply.github.com>
2024-08-01 18:20:53 -0600

    feat: fill out routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import {
 	authController,
 	userController,
@@ -7,35 +7,35 @@ import {
 	likeController
 } from '../controllers/controllers';
 
-const router = express.Router();
+const router: Router = express.Router();
 
 // AUTH ROUTES
 router.post('/login', authController.login);
 router.delete('/logout', authController.logout);
 
 // USER ROUTES
-router.get('/:username',);
-router.post('/:username',);
-router.put('/:username',);
+router.get('/:username', userController.getUser);
+router.post('/signup', userController.createUser);
+router.put('/:username/:update', userController.updateUser);
 
 // POST ROUTES
-router.get('/home',);
-router.get('/:username/posts');
-router.get('/:username/:postId',);
-router.post('/:username/:postId',);
-router.put('/:username/:postId',);
-router.delete('/:username/:postId',);
+router.get('/home', postController.home);
+router.get('/:username/posts', postController.getUserPosts);
+router.get('/:username/:postId', postController.getPost);
+router.post('/:username/:postId', postController.createPost);
+router.put('/:username/:postId', postController.updatePost);
+router.delete('/:username/:postId', postController.deletePost);
 
 // COMMENT ROUTES
-router.get('/:username/comments');
-router.get('/:username/:commentId',);
-router.post('/:username/:commentId',);
-router.put('/:username/:commentId',);
-router.delete('/:username/:commentId',);
+router.get('/:username/comments', commentController.getUserComments);
+router.get('/:username/:commentId', commentController.getComment);
+router.post('/:username/:commentId', commentController.createComment);
+router.put('/:username/:commentId', commentController.updateComment);
+router.delete('/:username/:commentId', commentController.deleteComment);
 
 // LIKE ROUTES
-router.get('/:username/likes');
-router.post('/:username/:likeId',);
-router.delete('/:username/:likeId',);
+router.get('/:username/likes', likeController.getUserLikes);
+router.post('/:username/:likeId', likeController.createLike);
+router.delete('/:username/:likeId', likeController.deleteLike);
 
-module.exports = router;
+export default router;
