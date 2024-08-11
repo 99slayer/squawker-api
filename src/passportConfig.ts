97f1f10ce1c332659974888a8a6c71eae1017d37
99slayer <21979862+99slayer@users.noreply.github.com>
2024-08-10 19:08:46 -0600

    feat: Add bcrypt for hash checking

--- src/passportConfig.ts
+++ src/passportConfig.ts
@@ -1,23 +1,27 @@
 const LocalStrategy = require('passport-local').Strategy;
 import User from './models/user';
 import passport, { DoneCallback as dcb } from 'passport';
+import bcrypt from 'bcryptjs';
+import { PopulatedDoc } from 'mongoose';
+import { UserInterface } from './types';
 
 export function initialize(passport: passport.PassportStatic) {
-	const authenticateUser = async (username: string, password: string, done: dcb): Promise<void> => {
-		const user = await User.findOne({ username: username });
+	const authenticateUser = async (
+		username: string,
+		password: string,
+		done: dcb
+	): Promise<void> => {
+		const user: PopulatedDoc<UserInterface> | null = await User.findOne({ username: username });
 
 		if (!user) {
-			console.log('user does not exist');
 			return done(null, false);
 		}
 
 		try {
-			if (password === user.password) {
-				console.log('authenticated');
-				console.log(user);
+			if (!('password' in user)) return done(null, false);
+			if (await bcrypt.compare(password, user.password)) {
 				return done(null, user);
 			} else {
-				console.log('wrong password');
 				return done(null, false);
 			}
 		} catch (error) {
@@ -30,15 +34,11 @@ export function initialize(passport: passport.PassportStatic) {
 	);
 
 	passport.serializeUser((user: any, done: dcb): void => {
-		console.log('serialize');
-
-		done(null, user.id);
+		done(null, user._id);
 	});
 
 	passport.deserializeUser(async (id: string, done: dcb): Promise<void> => {
-		console.log('deserialize');
-
-		const user = await User.findById(id);
+		const user: PopulatedDoc<UserInterface> | null = await User.findById(id);
 		done(null, user);
 	});
 }
