0cf98c49551f15edc65a62d3e00a5988190a39f4
99slayer <21979862+99slayer@users.noreply.github.com>
2024-08-13 16:15:45 -0600

    refactor: Update route urls

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -21,21 +21,21 @@ router.put('/:username/update-:update', userController.updateUser);
 // POST ROUTES
 router.get('/home', postController.home);
 router.get('/:username/posts', postController.getUserPosts);
-router.get('/:username/status/:postId', postController.getPost);
+router.get('/:username/post/:postId', postController.getPost);
 router.post('/publish-post', postController.createPost);
 router.put('/edit-post/:postId', postController.updatePost);
 router.delete('/delete-post/:postId', postController.deletePost);
 
 // COMMENT ROUTES
 router.get('/:username/comments', commentController.getUserComments);
-router.get('/:username/status/:commentId', commentController.getComment);
-router.post('/:username/publish-comment', commentController.createComment);
-router.put('/:username/edit-:commentId', commentController.updateComment);
-router.delete('/:username/delete-:commentId', commentController.deleteComment);
+router.get('/:username/comment/:commentId', commentController.getComment);
+router.post('/publish-comment/:parentType-:parentId', commentController.createComment);
+router.put('/edit-comment/:commentId', commentController.updateComment);
+router.delete('/delete-comment/:commentId', commentController.deleteComment);
 
 // LIKE ROUTES
 router.get('/:username/likes', likeController.getUserLikes);
-router.post('/:username/like', likeController.createLike);
-router.delete('/:username/delete-:likeId', likeController.deleteLike);
+router.post('/publish-like/:postId', likeController.createLike);
+router.delete('/delete-like/:likeId', likeController.deleteLike);
 
 export default router;
