05c6b4e0037c0b0217df23676fe28cccdc0a5d9d
99slayer <21979862+99slayer@users.noreply.github.com>
2024-08-17 17:53:43 -0600

    refactor: Add error handler to routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,41 +1,117 @@
 import express, { Router } from 'express';
 import {
-	authController,
-	userController,
-	postController,
-	commentController,
-	likeController
+	auth,
+	user,
+	post,
+	comment,
+	like
 } from '../controllers/controllers';
+import err from '../handleError';
 
 const router: Router = express.Router();
 
 // AUTH ROUTES
-router.post('/login', authController.login);
-router.delete('/logout', authController.logout);
+router.post(
+	'/login',
+	auth.login,
+	err
+);
+router.delete(
+	'/logout',
+	auth.logout,
+	err
+);
 
 // USER ROUTES
-router.get('/user/:username', userController.getUser);
-router.post('/signup', userController.createUser);
-router.put('/:username/update-:update', userController.updateUser);
+router.get(
+	'/user/:username',
+	user.getUser,
+	err
+);
+router.post(
+	'/signup',
+	user.createUser,
+	err
+);
+router.put(
+	'/:username/update-:update',
+	user.updateUser,
+	err
+);
 
 // POST ROUTES
-router.get('/home', postController.home);
-router.get('/:username/posts', postController.getUserPosts);
-router.get('/:username/post/:postId', postController.getPost);
-router.post('/publish-post', postController.createPost);
-router.put('/edit-post/:postId', postController.updatePost);
-router.delete('/delete-post/:postId', postController.deletePost);
+router.get(
+	'/home',
+	post.home,
+	err
+);
+router.get(
+	'/:username/posts',
+	post.getUserPosts,
+	err
+);
+router.get(
+	'/:username/post/:postId',
+	post.getPost,
+	err
+);
+router.post(
+	'/publish-post/:quotedPostId?',
+	post.createPost,
+	err
+);
+router.put(
+	'/edit-post/:postId',
+	post.updatePost,
+	err
+);
+router.delete(
+	'/delete-post/:postId',
+	post.deletePost,
+	err
+);
 
 // COMMENT ROUTES
-router.get('/:username/comments', commentController.getUserComments);
-router.get('/:username/comment/:commentId', commentController.getComment);
-router.post('/publish-comment/:parentType-:parentId', commentController.createComment);
-router.put('/edit-comment/:commentId', commentController.updateComment);
-router.delete('/delete-comment/:commentId', commentController.deleteComment);
+router.get(
+	'/:username/comments',
+	comment.getUserComments,
+	err
+);
+router.get(
+	'/:username/comment/:commentId',
+	comment.getComment,
+	err
+);
+router.post(
+	'/publish-comment/:parentType-:parentId',
+	comment.createComment,
+	err
+);
+router.put('/edit-comment/:commentId',
+	comment.updateComment,
+	err
+);
+router.delete(
+	'/delete-comment/:commentId',
+	comment.deleteComment,
+	err
+);
 
 // LIKE ROUTES
-router.get('/:username/likes', likeController.getUserLikes);
-router.post('/publish-like/:postId', likeController.createLike);
-router.delete('/delete-like/:likeId', likeController.deleteLike);
+router.get(
+	'/:username/likes',
+	like.getUserLikes,
+	err
+);
+router.post(
+	'/publish-like/:postId',
+	like.createLike,
+	err
+);
+router.delete(
+	'/delete-like/:likeId',
+	like.deleteLike,
+	err
+);
 
 export default router;
